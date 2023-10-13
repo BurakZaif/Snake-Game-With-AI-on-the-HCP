15d3dfcea9bedc98000e94465d3da6105963aadb
BurakZaif <burakzaiff@gmail.com>
2023-10-13 10:56:09 +0300

    Last version

--- main.py
+++ main.py
@@ -4,77 +4,65 @@ import threading
 import random
 from rich import print as rprint
 from rich.console import Console
+import os
+import heapq
 
 console = Console(force_terminal=True)
 
-rows, cols = 12, 12
+rows, cols = 22, 22
 arr = [[" " for i in range(cols)] for j in range(rows)]
 queue = []
 first = True
 Init_tup = (1, 3)
-interrupt = "d"
+flag = False
+values = [-1,1]
+x_count = 0
+y_count = 0
 
-interrupt_lock = threading.Lock()
-
-for i in range(1, 11):
+for i in range(1, 21):
     arr[0][i] = "~"
-    arr[11][i] = "~"
-for j in range(1, 11):
+    arr[21][i] = "~"
+for j in range(1, 21):
     arr[j][0] = "|"
-    arr[j][11] = "|"
+    arr[j][21] = "|"
 
 arr[0][0] = "+"
 arr[0][11] = "+"
-arr[11][0] = "+"
-arr[11][11] = "+"
+arr[21][0] = "+"
+arr[21][21] = "+"
 
-def process_key_events():
-    global interrupt
-    while True:
-        if keyboard.is_pressed('w'):
-            interrupt = "w"
-            print("\n")
-            # Do something based on the 'w' key press
-        elif keyboard.is_pressed('a'):
-            interrupt = "a"
-            print("\n")
-            # Do something based on the 'a' key press
-        elif keyboard.is_pressed('s'):
-            interrupt = "s"
-            print("\n")
-            # Do something based on the 's' key press
-        elif keyboard.is_pressed('d'):
-            interrupt = "d"
-            print("\n")
-            # Do something based on the 'd' key press
-
-        time.sleep(0.1)  # Add a small delay to avoid excessive checking
-
-keyboard_thread = threading.Thread(target=process_key_events)
-keyboard_thread.start()
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def printPlane():
+    output = ""
+
     for row in arr:
         for element in row:
             if element == "#":
-                console.print("[green]" + str(element), end=' ')
+                output += "\033[32m" + str(element) + " "  # Green color
             elif element in ['+', '|', '~']:
-                console.print("[red]" + str(element), end=' ')
+                output += "\033[31m" + str(element) + " "  # Red color
             else:
-                console.print(element, end=' ')
-        console.print()
+                output += str(element) + " "
+        output += "\033[0m\n"  # Reset color and move to the next line
+
+    clear_terminal()  # Clear the terminal
+    print("\033[H" + output)  # Move cursor to the top and print the entire output
+    print("SCORE:", len(queue))
+
 
 
 def snakeInit():
-    for i in range(1, 4):
+    for i in range(1,4):
+        queue.append((1,i))
         arr[1][i] = "#"
 
-def playerMove():
-    move = input("Sıradaki hamle..")
 
 def checkBoundary(tuple):
-    if arr[tuple[0]][tuple[1]] == "|" or arr[tuple[0]][tuple[1]] == "~" or arr[tuple[0]][tuple[1]] == "#":
+    if tuple == None or arr[tuple[0]][tuple[1]] == "|" or arr[tuple[0]][tuple[1]] == "~" or arr[tuple[0]][tuple[1]] == "#" :
         print("!!GAME OVER!!")
+        printPlane()
         return False
     return True
 
@@ -84,83 +72,128 @@ def checkBox(x, y):
     else:
         return True
 
+def checkBoxMove(x, y, dir):
+    global values, x_count, y_count
+    mylocal = y
+    initmove = random.choice(values)
+    if arr[x][y] == "#" and dir == "y":
+        mylocal = initmove + y
+        return checkBoxMove(x,mylocal, "x")
+    else:
+        if  initmove == 1 and y_count < 0:
+            y_count = y_count -1
+        elif initmove == 1 and y_count > 0:
+            y_count = y_count + 1
+        elif initmove == -1 and y_count > 0:
+            y_count = y_count + 1
+        else :
+            y_count = y_count - 1
+        y = mylocal
+        return (x,y)
+
+def mynew(new_x):
+    global Init_tup, y_count
+    local = random.choice(values)
+    Init_tup = (new_x, Init_tup[1] + local)
+    if arr[Init_tup[0]][Init_tup[1]] == "#":
+        mynew(new_x)
+    else:
+        if local == 1 and y_count < 0:
+            y_count = y_count - 1
+        elif local == 1 and y_count > 0:
+            y_count = y_count + 1
+        elif local == -1 and y_count > 0:
+            y_count = y_count + 1
+        else:
+            y_count = y_count - 1
+
 def fruitSpawn():
-    random_x = random.randint(1,11)
-    random_y = random.randint(1,11)
+    random_x = random.randint(1,21)
+    random_y = random.randint(1,21)
     if(checkBox(random_x, random_y)):
-        return (random_x, random_y)
+        arr[random_x][random_y] = "$"
+        point = (random_x,random_y)
+        return point
     else:
         return fruitSpawn()
 
-def spawnPoint(currentFruit):
-    if(currentFruit):
-        point = fruitSpawn()
-        arr[point[0]][point[1]] = "$"
-    return False
-
-def checkFruit(mytuple):
-    print("Girdimm")
-    print(arr[mytuple[0]][mytuple[1]])
-    if arr[mytuple[0]][mytuple[1]] == "$":
-        print("I am inside ")
-        return True
-    return False
-
-
 def printQueue(queue):
-    temp_queue = list(queue)  # Create a temporary list with the contents of the queue
+    temp_queue = list(queue)
     for item in temp_queue:
         print(item)
 
-def snakeMove(isEat):
+
+def manhattan_single(p1,p2):
+    return p1-p2
+
+
+def minimove(fruitPoint,head):
+    global Init_tup, flag, x_count, y_count
+    x_count = manhattan_single(fruitPoint[0], head[0])
+    y_count = manhattan_single(fruitPoint[1], head[1])
+    i = abs(x_count)
+    j = abs(y_count)
+    while (i != 0):
+        next_body = queue.pop(0)
+        x = next_body[0]
+        y = next_body[1]
+        if (x_count < 0):
+            new_x = Init_tup[0] - 1
+        else:
+            new_x = Init_tup[0] + 1
+
+        Init_tup = (new_x, Init_tup[1])
+        if arr[Init_tup[0]][Init_tup[1]] == "#":
+            Init_tup = mynew(new_x)
+
+        if checkBoundary(Init_tup) == False:
+            flag = True
+            return
+        arr[x][y] = " "
+        queue.append(Init_tup)
+        arr[Init_tup[0]][Init_tup[1]] = "#"
+        printPlane()
+        time.sleep(0.1)
+        i -= 1
+    while (j != 0):
+        next_body = queue.pop(0)
+        x = next_body[0]
+        y = next_body[1]
+        if (y_count < 0):
+            new_y = Init_tup[1] - 1
+        else:
+            new_y = Init_tup[1] + 1
+
+        Init_tup = (Init_tup[0], new_y)
+        if checkBoundary(Init_tup) == False:
+            flag = True
+            return
+        arr[x][y] = " "
+        queue.append(Init_tup)
+        arr[Init_tup[0]][Init_tup[1]] = "#"
+        printPlane()
+        time.sleep(0.1)
+        j -= 1
+    queue.insert(0, next_body)
+
+
+
+
+def snakeMove():
     global Init_tup, interrupt
     while (True):
         if (first):
-            with interrupt_lock:
-                next_body = queue.pop(0)
-                x = next_body[0]
-                y = next_body[1]
-                print("son" + str(x) + str(y))
-                if(interrupt == "s"):
-                    new_x = Init_tup[0] + 1
-                    new_y = Init_tup[1]
-                elif interrupt == "w":
-                    new_x = Init_tup[0] - 1
-                    new_y = Init_tup[1]
-                elif interrupt == "d":
-                    new_x = Init_tup[0]
-                    new_y = Init_tup[1] + 1
-                elif interrupt == "a":
-                    new_x = Init_tup[0]
-                    new_y = Init_tup[1] - 1
-                Init_tup = (new_x, new_y)
-                if checkBoundary(Init_tup) == False:
-                    return
-                print("baş" + str(new_x) + str(new_y))
-                if checkFruit(Init_tup):
-                    print("burdayımmm")
-                    isEat = True
-                    queue.insert(0, next_body)
-                    queue.append(Init_tup)
-                else:
-                    arr[x][y] = " "
-                    queue.append(Init_tup)
-                arr[Init_tup[0]][Init_tup[1]] = "#"
-                printQueue(queue)
-                time.sleep(1)
-                printPlane()
-                print(isEat)
-                isEat = spawnPoint(isEat)
-                time.sleep(0.2)
+            if(flag == True):
+                return
+            minimove(fruitSpawn(), (Init_tup[0],Init_tup[1]))
+            #printQueue(queue)
+            time.sleep(0.21)
+
+
 
 def game():
-    isEat = True
-    queue.append((1, 1))
-    queue.append((1, 2))
-    queue.append((1, 3))
     snakeInit()
     printPlane()
-    snakeMove(isEat)
-
+    snakeMove()
 
 game()
